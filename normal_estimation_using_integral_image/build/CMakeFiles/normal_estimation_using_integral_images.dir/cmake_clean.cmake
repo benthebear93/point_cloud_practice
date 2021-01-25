6c0ee0a411b0e3c9bcc327e39c5f255d3793eed6
benthebear93 <nswve@naver.com>
2021-01-25 22:56:16 +0900

    normal estimation using integral image

---
+++ normal_estimation_using_integral_image/build/CMakeFiles/normal_estimation_using_integral_images.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/normal_estimation_using_integral_images.dir/normal_estimation_using_integral_images.cpp.o"
+  "normal_estimation_using_integral_images.pdb"
+  "normal_estimation_using_integral_images"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/normal_estimation_using_integral_images.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
