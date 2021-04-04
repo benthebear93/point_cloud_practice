5d1d4d1784ce77eb76dcb450b0caaceb7695e5e9
benthebear93 <nswve@naver.com>
2021-04-04 17:56:56 +0900

    downsamoling added

---
+++ downsampling_voxel/build/CMakeFiles/downsampling_voxel.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/downsampling_voxel.dir/downsampling_voxel.cpp.o"
+  "downsampling_voxel.pdb"
+  "downsampling_voxel"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/downsampling_voxel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- downsampling_voxel/downsampling_voxel.cpp
+++ downsampling_voxel/downsampling_voxel.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
-#include <pcl/filters/voxel_gird.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/visualization/cloud_viewer.h>
 
+int user_data = 0;
 int
 main (int argc, char** argv)
 {
-  pcl::PLCPointCloud2::Ptr cloud (new pcl::PLCPointCloud2 ());
-  pcl::PLCPointCloud2::Ptr cloud_filtered (new pcl::PLCPointCloud2 ());
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
 
   pcl::io::loadPCDFile ("/home/benlee/Desktop/pc_practice/normal_estimation_using_integral_image/table_scene_mug_stereo_textured.pcd", *cloud);
-  pcl::VoxelGrid<pcl::PLCPointCloud2> sor;
+  pcl::VoxelGrid<pcl::PointXYZ> sor;
   sor.setInputCloud (cloud);
-  sor.setLeafSize (0.01f, 0.01f, 0.01f);
-  sor.filter (*cloud_filtered)
-
-  std::cerr << "Point cloud after filtering" << cloud_filtered->width * cloud_filtered->height << "data points (" << pcl::getFieldList (*cloud_filtered) <<")."<<std::endl;
-    
+  sor.setLeafSize (0.05f, 0.05f, 0.05f);
+  sor.filter (*cloud_filtered);
+  
+    pcl::visualization::CloudViewer viewer("Cloud Viewer");
+    viewer.showCloud(cloud_filtered);
+    while (!viewer.wasStopped ())
+    {
+        user_data++;
+    }
+    return 0;
 }
