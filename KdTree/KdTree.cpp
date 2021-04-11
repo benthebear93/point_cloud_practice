c8cd2d39e8133f8eac8cee47ce380c24776a142f
benthebear93 <nswve@naver.com>
2021-04-11 17:02:06 +0900

    Basic KdTree

---
+++ KdTree/KdTree.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/point_cloud.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <vector>
+#include <ctime>
+
+int user_data = 0;
+int main (int argc, char** argv)
+{
+  srand (time(NULL));
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+
+  pcl::io::loadPCDFile ("/home/benlee/Desktop/pc_practice/normal_estimation_using_integral_image/table_scene_mug_stereo_textured.pcd", *cloud);
+  pcl::KdTreeFLANN<pcl::PointXYZ> kdtree;
+
+  kdtree.setInputCloud (cloud);
+  pcl::PointXYZ searchPoint;
+
+  searchPoint.x = 1024.0f * rand () / (RAND_MAX + 1.0f);
+  searchPoint.y = 1024.0f * rand () / (RAND_MAX + 1.0f);
+  searchPoint.z = 1024.0f * rand () / (RAND_MAX + 1.0f);
+
+  int K = 10;
+  std::vector<int> pointIdxNKNSearch(K);
+  std::vector<float> pointNKNSquaredDistance(K);
+
+  std::cout << "K nearest neighbor search at (" << searchPoint.x 
+            << " " << searchPoint.y 
+            << " " << searchPoint.z
+            << ") with K=" << K << std::endl;
+  if ( kdtree.nearestKSearch (searchPoint, K, pointIdxNKNSearch, pointNKNSquaredDistance) > 0 )
+  {
+    for (std::size_t i = 0; i < pointIdxNKNSearch.size (); ++i)
+      std::cout << "    "  <<   (*cloud)[ pointIdxNKNSearch[i] ].x 
+                << " " << (*cloud)[ pointIdxNKNSearch[i] ].y 
+                << " " << (*cloud)[ pointIdxNKNSearch[i] ].z 
+                << " (squared distance: " << pointNKNSquaredDistance[i] << ")" << std::endl;
+  }
+
+  std::vector<int> pointIdxRadiusSearch;
+  std::vector<float> pointRadiusSquaredDistance;
+  float radius = 256.0f * rand () / (RAND_MAX + 1.0f);
+
+  std::cout << "Neighbors within radius search at (" << searchPoint.x 
+            << " " << searchPoint.y 
+            << " " << searchPoint.z
+            << ") with radius=" << radius << std::endl;
+
+    pcl::visualization::CloudViewer viewer("Cloud Viewer");kdtree.setInputCloud(cloud);
+
+  if ( kdtree.radiusSearch (searchPoint, radius, pointIdxRadiusSearch, pointRadiusSquaredDistance) > 0 )
+  {
+    for (std::size_t i = 0; i < pointIdxRadiusSearch.size (); ++i)
+      std::cout << "    "  <<   (*cloud)[ pointIdxRadiusSearch[i] ].x 
+                << " " << (*cloud)[ pointIdxRadiusSearch[i] ].y 
+                << " " << (*cloud)[ pointIdxRadiusSearch[i] ].z 
+                << " (squared distance: " << pointRadiusSquaredDistance[i] << ")" << std::endl;
+  }
+
+    // viewer.showCloud(cloud_filtered);
+    // while (!viewer.wasStopped ())
+    // {
+    //     user_data++;
+    // }
+    // return 0;
+}
---
+++ KdTree/build/CMakeFiles/KdTree.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KdTree.dir/KdTree.cpp.o"
+  "KdTree.pdb"
+  "KdTree"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KdTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
