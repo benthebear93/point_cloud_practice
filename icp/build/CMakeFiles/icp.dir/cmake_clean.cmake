6b60e4357d58435b2e0605e669a7e13f3471b5b9
benthebear93 <nswve@naver.com>
2021-04-16 10:33:56 +0900

    icp

---
+++ icp/build/CMakeFiles/icp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/icp.dir/icp.cpp.o"
+  "icp.pdb"
+  "icp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/icp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
